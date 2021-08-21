0ed87813e17b6c8048b24f30588f296b280d78af
nujhatfariha049 <nujhatfarihaa@gmail.com>
2021-08-21 22:09:25 +0600

    added promo code

--- js/script.js
+++ js/script.js
@@ -1,99 +1,81 @@
-//  id call for memory
+//  id call
 const memory8Gb = document.getElementById("memory-8gb");
 const memory16Gb = document.getElementById("memory-16gb");
 const memoryCostField = document.getElementById("extra-memory-cost");
-//id call for storage
+
 const storage256Gb = document.getElementById("storge-265gb");
 const storage512Gb = document.getElementById("storage-512gb");
 const storage1Tb = document.getElementById("storage-1tb");
 const storageCostField = document.getElementById("extra-storage-cost");
-//id call for delivery option
+
 const freeDelivery = document.getElementById("free-delivery");
 const deliveryChargeRequired = document.getElementById("delivery-charge-required")
 const deliveryChargeField = document.getElementById("delivery-charge");
-//id call for best price
+
 const bestPriceField = document.getElementById("best-price")
-// id call for total price
 const total = document.getElementById("total-price");
 let grandTotal = document.getElementById("grand-total-price");
 updateTotal()
 
-
 // update on the html
-memory8Gb.addEventListener("click", function (){
+memory8Gb.addEventListener("click", function () {
     memoryCostField.innerText = "0";
+    updateTotal()
 });
-memory16Gb.addEventListener("click", function (){
+memory16Gb.addEventListener("click", function () {
     memoryCostField.innerText = "180";
     updateTotal()
 })
-storage256Gb.addEventListener("click", function (){
+storage256Gb.addEventListener("click", function () {
     storageCostField.innerText = "0";
+    updateTotal()
 });
-storage512Gb.addEventListener("click", function (){
+storage512Gb.addEventListener("click", function () {
     storageCostField.innerText = "100";
     updateTotal()
 })
-storage1Tb.addEventListener("click", function (){
+storage1Tb.addEventListener("click", function () {
     storageCostField.innerText = "180";
     updateTotal()
 })
-freeDelivery.addEventListener("click", function (){
+freeDelivery.addEventListener("click", function () {
     deliveryChargeField.innerText = "0";
+    updateTotal()
 })
-deliveryChargeRequired.addEventListener("click", function (){
+deliveryChargeRequired.addEventListener("click", function () {
     deliveryChargeField.innerText = "20";
     updateTotal()
 })
 
 // total price update function
-function updateTotal(){
+function updateTotal() {
     const bestPrice = parseInt(bestPriceField.innerText);
     const extraMemoryCost = parseInt(memoryCostField.innerText);
     const extraStorageCost = parseInt(storageCostField.innerText);
-    const deliveryCharge = parseInt (deliveryChargeField.innerText);
-    const totalPrice = bestPrice + extraMemoryCost +extraStorageCost + deliveryCharge ;
+    const deliveryCharge = parseInt(deliveryChargeField.innerText);
+    const totalPrice = bestPrice + extraMemoryCost + extraStorageCost + deliveryCharge;
     total.innerText = totalPrice;
     grandTotal.innerText = totalPrice;
 }
 
-
-// // const promoCodeApplyField = document.getElementById('promo-code-apply')
-
-// // promoCodeApplyField.addEventListener('click', function () {
-// //     // get promo code
-// //     const promoCodeInputField = document.getElementById('promo-code-input');
-// //     const promoCode = promoCodeInputField.value;
+// promo code function
+function getPromoCode(){
+    document.getElementById('promo-code-apply').addEventListener('click', function () {
+        const promoCodeField = document.getElementById('promo-code-input');
+        const promoCode = promoCodeField.value;
+        const bestPrice = parseInt(bestPriceField.innerText);
+        const extraMemoryCost = parseInt(memoryCostField.innerText);
+        const extraStorageCost = parseInt(storageCostField.innerText);
+        const deliveryCharge = parseInt (deliveryChargeField.innerText);
+        const totalPrice = bestPrice + extraMemoryCost +extraStorageCost + deliveryCharge ;
+        grandTotal.innerText = totalPrice;
+        if (promoCode == 'stevekaku') {
+            grandTotal.innerText = parseInt(totalPrice * 0.8);
+        } else{
+            promoCodeField.value = '';
+        }  
+        promoCodeField.value = '';
+    }) 
     
-// // });
-
-// // function getPromoCode(){
-// //     const promoCode = promoCodeInputField.value;
-// //      // check promo code
-// //      if (promoCode == 'stevekaku') {
-// //         grandTotal = totalPrice * 0.8;
-// //     }
-// // }
-
-
-// const promoCodeApplyField = document.getElementById("promo-code-apply");
-// getPromoCode()
-
-// promoCodeApplyField.addEventListener("click", function (){
-//     promoCodeInputField.value = promoCode;
-//     const promoCode = promoCodeInputField.value;
-//     getPromoCode()
-// })
-// function getPromoCode(){
-//         cons grandTotalPrice= grandTotal.innerText
-//          // check promo code
-//          if (promoCode == 'stevekaku') {
-//            grandTotal = totalPrice * 0.8;
-//         }else{
-//             return "you are not eligible for the discount"
-//         }
-//     }
-//     getPromoCode()
-
-
-
+}
+getPromoCode()
